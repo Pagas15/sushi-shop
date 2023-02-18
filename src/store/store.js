e4dd44a40c2a2256556e8284597e2bea53c7b5bd
Pagas15 <57036145+Pagas15@users.noreply.github.com>
2023-02-18 19:07:12 +0200

    added synchronization with localstorage through the redux-pesist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import filterSlice from './slices/filterSlice';
 import sushiSlice from './slices/sushiSlice';
 import userSlice from './slices/userSlice';
 
+const reducer = { sushi: sushiSlice, filters: filterSlice, user: userSlice };
+
+const rootReducer = combineReducers(reducer);
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['sushi'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { sushi: sushiSlice, filters: filterSlice, user: userSlice },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
